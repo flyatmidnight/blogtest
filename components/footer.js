cf9cf1fc18cfc55e8f86a3e0bc1577a8d3f7f485
Dom Eccleston <domeccleston@gmail.com>
2022-05-02 20:01:50 +0000

    fix footer color

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className="absolute bottom-0 w-full">
-      <div className="py-6 max-w-2xl mx-auto text-center border-t border-dotted">
+      <div className="py-6 max-w-2xl mx-auto text-center border-t border-dotted border-gray-500">
         <a href="mailto:domeccleston@gmail.com">Email</a> •{' '}
         <a href="https://twitter.com/dom__inic" target="_blank" rel="noopener">
           Twitter
